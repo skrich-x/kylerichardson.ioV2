869e9bb4aa8665d06535063da1e33062e40c8934
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-17 01:29:36 -0400

    adding tooltip for skills section and Toastr

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,17 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+
+// //Toaster Call//
+//  $(document).ready(function() {
+//      toastr.options.timeOut = 1500; // 1.5s
+//      toastr.info('Page Loaded!');
+//      $('#logo').click(function() {
+//         toastr.success('Click Button');
+//      });
+//    });
+
+//tooltipster js//
+  $(document).ready(function() {
+    $('.tooltip').tooltipster();
+  });
